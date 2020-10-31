97842d34c57870ffd8ff72b001f005c853d3ad29
bak1roff01 <bakirovgulomjon@gmail.com>
2020-10-31 10:08:47 +0300

    Create responsive Visit  section

--- js/app.js
+++ js/app.js
@@ -18,6 +18,19 @@ $(document).ready(function () {
     }
     );
   })
+  $('.btn1').click(function () {
+    $('.btn2').removeClass('visit--active');
+    $('.btn1').addClass('visit--active');
+    $('.visit-section-bottom__slider-2').css("display" , "none");
+    $('.visit-section-bottom__slider-1').css("display" , "flex");
+  });
+  $('.btn2').click(function () {
+    $('.btn1').removeClass('visit--active');
+    $('.btn2').addClass('visit--active');
+    $('.visit-section-bottom__slider-1').css("display" , "none");
+    $('.visit-section-bottom__slider-2').css("display" , "flex");
+  });
+
   
 
 
