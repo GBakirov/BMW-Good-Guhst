db5ed6c392e12fd3f0da2d9ad45a628def7cdfbe
bak1roff01 <bakirovgulomjon@gmail.com>
2020-10-31 16:55:51 +0300

    Create loader

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
 $(document).ready(function () {
+  AOS.init();
+
+  $(window).on("load", function(){
+  $('.loader').fadeOut('slow')
+  })
+
   $('#menu-toggle').click(function () {
     this.classList.toggle('active');
     $('.navbar__list').toggleClass('navbar-open');
     $('.navbar__list').toggleClass('navbar-close');
   });
+
   $('.navbar__item').click(function () {
     $('#menu-toggle').removeClass('active');
     $('.navbar__list').toggleClass('navbar-open');
     $('.navbar__list').toggleClass('navbar-close');
   });
+
   $(function(){
 
     $('.gallery-table2').slick({
@@ -18,12 +26,14 @@ $(document).ready(function () {
     }
     );
   })
+
   $('.btn1').click(function () {
     $('.btn2').removeClass('visit--active');
     $('.btn1').addClass('visit--active');
     $('.visit-section-bottom__slider-2').css("display" , "none");
     $('.visit-section-bottom__slider-1').css("display" , "flex");
   });
+
   $('.btn2').click(function () {
     $('.btn1').removeClass('visit--active');
     $('.btn2').addClass('visit--active');
